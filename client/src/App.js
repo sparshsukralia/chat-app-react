f05a9c044e9ba3cc82a71160df0f51c9cf99912b
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-17 17:34:52 +0545

    Join page css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./App.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Join from "./components/Join";
-import Chat from "./components/Chat";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 const App = () => {
   return(
